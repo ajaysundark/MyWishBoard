022bc8b87759aa4b4309ffcff61fadf683ea00ed
asundark <ajaysundar.k@gmail.com>
2014-09-19 14:58:05 +0530

    Terminate DBConnection after access.

--- src/main/java/service/db/BirthdayFetcher.java
+++ src/main/java/service/db/BirthdayFetcher.java
@@ -2,7 +2,6 @@ package main.java.service.db;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,56 +10,58 @@ import main.java.core.ColleagueBuilder;
 import main.java.core.ColleagueVO;
 import main.java.ifs.WishLogger;
 
-public class BirthdayFetcher {
-        private ResultSet todaysWishes;
-        private List<ColleagueVO> resultList = new ArrayList<ColleagueVO>();
-        private WishLogger logger;
+public class BirthdayFetcher
+{
+	private ResultSet todaysWishes;
+	private List<ColleagueVO> resultList = new ArrayList<ColleagueVO>();
+	private WishLogger logger;
 
-        DBConnector dbObject = new DBConnector();
-        private static final String mysqlQuery = "SELECT last_name, dob, designation, contact_no, email, cubicle"
-                        +" FROM data_table "
-                        +"WHERE ExtractMonth(dob) = ExtractMonth(CURRENT_DATE) "
+	DBConnector dbObject = new DBConnector();
+	private static final String mysqlQuery = "SELECT last_name, dob, designation, contact_no, email, cubicle" 
+	+ " FROM data_table " + "WHERE ExtractMonth(dob) = ExtractMonth(CURRENT_DATE) "	+ "AND ExtractDay(dob) = ExtractDay(CURRENT_DATE)";
 
-                        +"AND ExtractDay(dob) = ExtractDay(CURRENT_DATE)";
+	public List<ColleagueVO> fetchBirthdayColleagues()
+	{
+		if (dbObject == null) { return null; }
 
-        public List<ColleagueVO> fetchBirthdayColleagues() {
-                if(dbObject==null) {
-                        return null;
-                }
+		logger = dbObject.getMyLogger();
 
-                logger = dbObject.getMyLogger();
+		try
+		{
+			if (dbObject.initConnection())
+			{
+				Connection myConnection = dbObject.getDBConnection();
+				Statement stmt = myConnection.createStatement();
 
-                if (dbObject.initConnection()) {
-                        Connection myConnection = dbObject.getDBConnection();
-                        try {
-                                Statement stmt = myConnection.createStatement();
+				todaysWishes = stmt.executeQuery(mysqlQuery);
+				while (todaysWishes.next())
+				{
+					ColleagueVO birthdayColleague = ColleagueBuilder.aColleague()
 
-                                todaysWishes=stmt.executeQuery(mysqlQuery);
-                                while (todaysWishes.next()) {
-                                        ColleagueVO birthdayColleague = ColleagueBuilder.aColleague()
+					.addLastName(todaysWishes.getString(1))
 
-                                .addLastName(todaysWishes.getString(1))
+					.addDOB(todaysWishes.getDate(2))
 
-                                .addDOB(todaysWishes.getDate(2))
+					.addDesignation(todaysWishes.getString(3))
 
-                                .addDesignation(todaysWishes.getString(3))
+					.addContactNo(todaysWishes.getString(4))
 
-                                .addContactNo(todaysWishes.getString(4))
+					.addEmaidId(todaysWishes.getString(5))
 
-                                .addEmaidId(todaysWishes.getString(5))
+					.addLocation(todaysWishes.getString(6))
 
-                                .addLocation(todaysWishes.getString(6))
+					.build();
 
-                                .build();
+					resultList.add(birthdayColleague);
 
-                                        resultList.add(birthdayColleague);
-
-                                }
-                        } catch (SQLException e) {
-                                logger.logError("DB Access Error at " + getClass
-(), e);
-                        }
-                }
-                return resultList;
-        }
+				}
+			}
+		} catch (Exception e)
+		{
+			logger.logError("DB Access Error at " + getClass(), e);
+		} finally {
+			dbObject.terminateConnection();
+		}
+		return resultList;
+	}
 }
--- src/main/java/service/db/DBConnector.java
+++ src/main/java/service/db/DBConnector.java
@@ -10,79 +10,91 @@ import main.java.ifs.WishLogger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class DBConnector {
-        
-        private WishLogger myLogger = null;
+public class DBConnector
+{
 
-        private ApplicationContext dbContainer = null;
+	private WishLogger myLogger = null;
 
-        private Connection dbConnection;
+	private ApplicationContext dbContainer = null;
 
-        public DBConnector() {
-                dbContainer = new ClassPathXmlApplicationContext("WishBoard-DBbeanfactory.xml");
-                myLogger = (WishLogger) dbContainer.getBean("log-bean");
-        }
+	private Connection dbConnection;
 
-        public boolean initConnection() {
-                myLogger.logDebug("Trying to establish Connection to DB :");
-                ConnectionConfig config = (ConnectionConfig) dbContainer.getBean
-("dbconnection-bean");
+	public DBConnector() {
+		dbContainer = new ClassPathXmlApplicationContext("WishBoard-DBbeanfactory.xml");
+		myLogger = (WishLogger) dbContainer.getBean("log-bean");
+	}
 
-                String driver = config.getDriver();
-                String connectionURL = config.getConnectionURL();
-                
-                /**
-                 * No username and password since we changed to SQLite now!
-                 */
-                
-                //String user = config.getUser(); 
-                //String password = config.getPassword();
-                
-                if(!connectionURL.isEmpty()) {
-                        try {
-                                Class.forName(driver);
-                                myLogger.logDebug("Database Driver loaded.");
-                                myLogger.logDebug("Connecting to DB...");
-                                dbConnection = DriverManager.getConnection(connectionURL);
-                                myLogger.log("Connected to DB.");
-                                return true;
-                        } catch (ClassNotFoundException NotFound) {
-                                myLogger.logError("Driver not found : [ " + driver + " ]", NotFound);
-                                return false;
-                        } catch (SQLException sqlEx) {
-                                myLogger.logError("Connection to "+connectionURL
-+"failed!", sqlEx);
-                                return false;
-                        }
+	public boolean initConnection()
+	{
+		myLogger.logDebug("Trying to establish Connection to DB :");
+		ConnectionConfig config = (ConnectionConfig) dbContainer.getBean("dbconnection-bean");
 
-                } else {
-                        myLogger.logError("[Error :] Connection URL is EMPTY!");
+		String driver = config.getDriver();
+		String connectionURL = config.getConnectionURL();
 
-                        return false;
-                }
-        }
+		/**
+		 * No username and password since we changed to SQLite now!
+		 */
 
-        public Connection getDBConnection() {
-                return dbConnection;
-        }
+		// String user = config.getUser();
+		// String password = config.getPassword();
 
-        public boolean terminateConnection(Connection UsedDBSession) {
-                try {
-                        UsedDBSession.close();
-                        myLogger.log("DB Session ended - connection closed.");
-                } catch (SQLException e) {
-                        myLogger.logError("DB Connection problem!", e);
-                }
-                return true;
-        }
+		if (!connectionURL.isEmpty())
+		{
+			try
+			{
+				Class.forName(driver);
+				myLogger.logDebug("Database Driver loaded.");
+				myLogger.logDebug("Connecting to DB...");
+				dbConnection = DriverManager.getConnection(connectionURL);
+				myLogger.log("Connected to DB.");
+				return true;
+			} catch (ClassNotFoundException NotFound)
+			{
+				myLogger.logError("JDBC Driver not found : [ " + driver + " ]", NotFound);
+				return false;
+			} catch (SQLException sqlEx)
+			{
+				myLogger.logError("Connection to " + connectionURL + "failed!", sqlEx);
+				return false;
+			}
 
-        public WishLogger getMyLogger() {
-                return myLogger;
-        }
+		} else
+		{
+			myLogger.logError("[Error :] Connection URL is EMPTY!");
 
-        public class DBLogger extends WishLogger {
-                public DBLogger(boolean debug) {
-                        super(debug);
-                }
-        }
+			return false;
+		}
+	}
+
+	public Connection getDBConnection()
+	{
+		return dbConnection;
+	}
+
+	public boolean terminateConnection()
+	{
+		Connection UsedDBSession = this.getDBConnection();
+		try
+		{
+			UsedDBSession.close();
+			myLogger.log("DB Session ended - connection closed.");
+		} catch (SQLException e)
+		{
+			myLogger.logError("DB Connection problem!", e);
+		}
+		return true;
+	}
+
+	public WishLogger getMyLogger()
+	{
+		return myLogger;
+	}
+
+	public class DBLogger extends WishLogger
+	{
+		public DBLogger(boolean debug) {
+			super(debug);
+		}
+	}
 }
